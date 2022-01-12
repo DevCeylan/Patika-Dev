a5558377f24a450c036d7332d11a5936e9f1ec28
Mehmet CEYLAN <ceylarino@gmail.com>
2022-01-12 14:08:48 +0300

    fix hw3 - add hw4

--- Sql/hw3.sql
+++ Sql/hw3.sql
@@ -1,6 +1,6 @@
 -- 1: country tablosunda bulunan country sütunundaki ülke isimlerinden 'A' karakteri ile başlayıp 'a' karakteri ile sonlananları sıralayınız.
 SELECT country FROM country
-WHERE country ILIKE 'A%a';
+WHERE country LIKE 'A%a';
 
 -- 2: country tablosunda bulunan country sütunundaki ülke isimlerinden en az 6 karakterden oluşan ve sonu 'n' karakteri ile sonlananları sıralayınız.
 SELECT country FROM country
@@ -12,4 +12,4 @@ WHERE title LIKE '%t%t%t%t%';
 
 -- 4: film tablosunda bulunan tüm sütunlardaki verilerden title 'C' karakteri ile başlayan ve uzunluğu (length) 90 dan büyük olan ve rental_rate 2.99 olan verileri sıralayınız.
 SELECT * FROM film
-WHERE title ILIKE 'C%' AND length > 90 AND rental_rate = 2.99;
+WHERE title LIKE 'C%' AND length > 90 AND rental_rate = 2.99;
